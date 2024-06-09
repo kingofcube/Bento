297013decdc27c048ee0de71abe686d89d488369
king_cube <138169081+kingofcube@users.noreply.github.com>
2024-06-09 17:00:32 -0400

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
 	name: 'M-Line',
 	imageBackground: false,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'Good morning!',
